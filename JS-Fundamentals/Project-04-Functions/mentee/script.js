fcec1294a2ba85dd8aaa5f31ea2e490696bedf1b
Jocelin Arriaga <jocelin.arriaga@yahoo.com>
2025-08-25 20:43:40 -0600

    functions and conditionals prac

--- JS-Fundamentals/Project-04-Functions/mentee/script.js
+++ JS-Fundamentals/Project-04-Functions/mentee/script.js
@@ -1,6 +1,93 @@
 // STEP 1: Create a function named sayHello that logs 'Hello!'
+function sayHello() {
+  console.log("Hello!");
+}
+sayHello();
 // STEP 2: Create a function addNumbers that adds 2 numbers and logs the result
+
+function addNumbers(a, b) {
+  return a + b;
+  //   console.log(a + b);
+}
+
+// addNumbers(3, 5);
+
+let result = addNumbers(3, 5);
+console.log(result);
+
 // STEP 3: Create a function greetUser that takes a name and says hello
+function greetUser(name) {
+  console.log("hello " + name);
+}
+
+greetUser("Alex");
+
 // STEP 4: Create a function that multiplies two numbers and logs it
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+let product = multiply(2, 2);
+console.log(product);
+
 // STEP 5: Create a function that logs your favorite quote
-// STEP 6: Call all the functions you created above
+function favoriteQuote() {
+  console.log("sample quote");
+}
+
+favoriteQuote();
+
+// STEP 6: Arrow function that subtracts two numbers
+const subtract = (a, b) => {
+  console.log(a - b);
+};
+
+subtract(10, 5);
+
+// STEP 7: Arrow function that divides two numbers
+const divide = (a, b) => {
+  console.log(a / b);
+};
+
+divide(10, 5);
+
+// STEP 8: Arrow function that checks if a number is even
+
+const evenNumber = (num) => {
+  if (num % 2 === 0) {
+    console.log("True");
+  } else {
+    console.log("False");
+  }
+};
+
+const isEven = (num) => (num % 2 === 0 ? "True" : "False");
+
+// ternary operator
+
+evenNumber(7);
+
+// STEP 9: Arrow function that returns a greeting with a name
+const greet = (name) => {
+  //   console.log("hi " + name);
+  console.log(`hi ${name}`);
+};
+
+greet("Diego");
+
+// STEP 10: Arrow function that squares a number
+
+const square = (num) => {
+  console.log(num ** 2);
+};
+
+square(5);
+
+// STEP 11: Arrow function that logs today’s date
+
+const todaysDate = () => {
+  //   console.log("8/25/2025");
+  console.log(new Date().toDateString());
+};
+
+todaysDate();
--- JS-Fundamentals/Project-05-Conditionals/mentee/script.js
+++ JS-Fundamentals/Project-05-Conditionals/mentee/script.js
@@ -1,12 +1,52 @@
 // STEP 1: Check if a score is greater than 80 and log 'Great job!'
-// STEP 2: Check if age is less than 18 and log 'You are a minor'
-// STEP 3: Use if/else to check if isHungry is true
-// STEP 4: Use if/else if/else to check grade level
-// STEP 5: Use an if to check if isLoggedIn is false
-// STEP 6: Use a conditional to check if temperature > 30
-
-let score = 85;
+let score = 80;
 
 if (score > 80) {
   console.log("Great Job!");
 }
+
+// STEP 2: Check if age is less than 18 and log 'You are a minor'
+let age = 17;
+
+if (age < 18) {
+  console.log("You are a minor");
+}
+
+// STEP 3: Use if/else to check if isHungry is true
+let isHungry = true;
+
+if (isHungry) {
+  console.log("true");
+} else {
+  console.log("false");
+}
+
+// STEP 4: Use if/else if/else to check grade level
+
+let grade = 50;
+
+if (grade >= 90) {
+  console.log("You got an A");
+} else if (grade >= 80) {
+  console.log("You got a B");
+} else {
+  console.log("You got a C, D, or F");
+}
+
+// STEP 5: Use an if to check if isLoggedIn is false
+let isLoggedIn = false;
+
+if (isLoggedIn) {
+  console.log("Logged in");
+} else {
+  console.log("Not logged in");
+}
+
+if (!isLoggedIn) {
+  console.log("Not logged in");
+}
+
+// STEP 6: Use a conditional to check if temperature > 30
+let temp = 15;
+
+console.log(temp > 30 ? "True" : "False");
